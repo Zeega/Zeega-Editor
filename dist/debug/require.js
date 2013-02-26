c32a2f6a243ebbe7670e685cfb2559d3cc650623
jbergen <jbergen@gmail.com>
2013-02-26 01:11:58 -0500

    release

--- dist/debug/require.js
+++ dist/debug/require.js
@@ -59549,11 +59549,11 @@ define('app',[
         // Allow LayoutManager to augment Backbone.View.prototype.
         manage: true,
 
-        prefix: "app/templates/",
+        // prefix: "app/templates/",
 
         fetch: function(path) {
             // Concatenate the file extension.
-            path = path + ".html";
+            path = "app/templates/" + path + ".html";
 
             // If cached, use the compiled template.
             if (JST[path]) {
