b0e42f33781a3c771fdb7e5c10bdbef71c92eea7
jbergen <jbergen@gmail.com>
2013-03-08 17:19:44 -0500

    added error when search returns no items

--- app/modules/views/media-collection-view.js
+++ app/modules/views/media-collection-view.js
@@ -18,19 +18,32 @@ function( app ) {
             return _.defaults( this.model.toJSON(), this.defaults );
         },
 
+        listen: null,
+
+        initialize: function() {
+            this.listen = _.once(function() {
+                this.model.mediaCollection.on("sync", this.render, this );
+            }.bind( this ));
+        },
+
         afterRender: function() {
+            console.log("AR", this.model.mediaCollection.length );
             this.$(".media-collection-items").empty();
-            this.model.mediaCollection.each(function( item ) {
-                this.$(".media-collection-items").append( item.view.el );
-                item.view.render();
-            }, this );
+
+            if ( this.model.mediaCollection.length ) {
+                this.model.mediaCollection.each(function( item ) {
+                    this.$(".media-collection-items").append( item.view.el );
+                    item.view.render();
+                }, this );
+            } else {
+                this.$(".media-collection-items").append("<div class='empty-collection'>no items found :( try again?</div>");
+            }
 
             // this.$(".media-collection-items")
             //     .append("<li class='media-more'><a href='#'><div class='item-label'>more</div><i class='icon-plus icon-white'></i></a></li>");
-
-            this.model.mediaCollection.on("sync", this.render, this );
+            
+            this.listen();
         }
-
 
     });
 
