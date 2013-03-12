6008b1e8f83b68c80478bdfeaef29e37f98dcaea
jbergen <jbergen@gmail.com>
2013-03-11 21:19:26 -0400

    added bookmarklet modal

--- app/modules/views/media-collection-view.js
+++ app/modules/views/media-collection-view.js
@@ -1,11 +1,14 @@
 define([
     "app",
+    "modules/views/modal",
     "backbone"
 ],
 
-function( app ) {
+function( app, Modal ) {
 
     return Backbone.View.extend({
+
+        bmModal: null,
 
         defaults: {
             title: "untitled"
@@ -42,8 +45,39 @@ function( app ) {
             //     .append("<li class='media-more'><a href='#'><div class='item-label'>more</div><i class='icon-plus icon-white'></i></a></li>");
             
             this.listen();
-        }
 
+            // show bookmarklet link
+            if ( this.model.get("title") == "My Media" ) {
+                this.$(".get-bookmarklet").show();
+            }
+        },
+
+        events: {
+            "click .get-bookmarklet": "bookmarkletModal"
+        },
+
+        bookmarkletModal: function() {
+            if ( this.bmModal === null ) {
+                this.bmModal = new Modal({
+                    modal: {
+                        title: "Get the Zeega Bookmarklet",
+                        className: "bookmarklet-modal",
+                        content: this.modalContent
+                    }
+                });
+            }
+
+            this.bmModal.show();
+        },
+
+        modalContent: "<div><p>Just drag this link to your browser's bookmark bar:</p></div>" +
+            "<div class='bmLink'><a href='javascript:(function()%7Bvar%20head=document.getElementsByTagName('body')%5B0%5D,script=document.createElement('script');script.id='zeegabm';script.type='text/javascript';script.src='//zeega.com/js/widget/zeega.bookmarklet.js?'%20+%20Math.floor(Math.random()*99999);head.appendChild(script);%7D)();%20void%200'>Add to Zeega</a></div>" +
+            "<div>" +
+                "<p>When you find something awesome that you want to use in your Zeega, click the bookmark and follow the simple instructions</p>" +
+                "<p class='small'><i class='icon-question-sign'></i> Don't see your bookmark bar? Go to View and select 'Always Show Bookmarks Bar'</p>" +
+            "</div>" +
+            "<img class='bm-instructions' src='assets/img/bookmarklet-arrow.png'/>"
+            
     });
 
 });
--- app/modules/views/modal.js
+++ app/modules/views/modal.js
@@ -12,17 +12,34 @@ function( app ) {
         modalClass: "",
         
         className: function() {
-            return "ZEEGA-modal " + this.modalClass;
+            console.log("cname", this.options.modal )
+            return "ZEEGA-modal " + this.options.modal.className;
+        },
+
+        serialize: function() {
+            console.log('this', this)
+            return this.options;
+        },
+
+        show: function() {
+            $("body").append( this.el );
+            $("#main").addClass("modal");
+            this.render();
         },
         
         events: {
-            "click .modal-close": "close"
+            "click .modal-close": "hide"
         },
 
         close: function() {
+            $("#main").removeClass("modal");
             this.$el.fadeOut(function() {
                 this.remove();
             }.bind( this ));
+        },
+
+        hide: function() {
+            this.close();
         }
 
     });
