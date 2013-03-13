6d23e15f3d3f46698b7583fa2026e7e8120f9b8c
jbergen <jbergen@gmail.com>
2013-03-13 00:46:22 -0400

    updated search url. omits www

--- app/app.js
+++ app/app.js
@@ -11,7 +11,7 @@ define([
 
         api: "",
         apiRoot: null,
-        searchAPI: "http://www.zeega.com/api/items/search?",
+        searchAPI: "http://zeega.com/api/items/search?",
         featuredAPI: "http://staging.zeega.org/api/items/featured",
 
         userId: null,
