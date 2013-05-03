a377dba0fe192f05c405794cc4260663c74abf40
James Burns <jameburn@gmail.com>
2013-05-02 17:40:23 -0700

    release

--- dist/debug/require.js
+++ dist/debug/require.js
@@ -77550,7 +77550,15 @@ function( Zeega, LayerModel, Visual ) {
         },
 
         onStateChange: function(e){
-            if( this.model.status.get("current_sequence_model").get("attr").soundtrack && /iPad/i.test(navigator.userAgent) && e.data ==2 && this.ignoreFirst ) {
+            var currentSequence;
+            if(this.model.status.get("current_sequence_model")){
+                currentSequence = this.model.status.get("current_sequence_model");
+            } else {
+                currentSequence = this.model.status.get("currentSequence");
+            }
+
+
+            if( currentSequence.get("attr").soundtrack && /iPad/i.test(navigator.userAgent) && e.data ==2 && this.ignoreFirst ) {
                 this.ignoreFirst = false;
                 this.ytPlayer.playVideo();
             } else if (e.data == 2 || e.data == 5){
@@ -77742,6 +77750,7 @@ function( app, Layers ) {
 
                 attr.soundtrack = newLayer.id;
                 this.set("attr", attr );
+                this.persistLayer( newLayer );
                 view.setSoundtrackLayer( newLayer );
 
                 this.lazySave();
@@ -77750,7 +77759,7 @@ function( app, Layers ) {
 
         removeSoundtrack: function( layer ) {
             var attr = this.get("attr");
-
+            this.unpersistLayer( layer );
             attr.soundtrack = false;
             this.set("attr", attr );
         },
