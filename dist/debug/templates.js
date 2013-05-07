9429585539415e5390ecb5650a86a9a9c2afac9a
jbergen <jbergen@gmail.com>
2013-05-07 15:09:08 -0700

    added @zeega to twitter share

--- dist/debug/templates.js
+++ dist/debug/templates.js
@@ -253,7 +253,7 @@ __p+='<div class="nav col-left navbar ZEEGA-hmenu clear">\n    <ul class=\'pull-
 ( webRoot )+
 ''+
 ( item_id )+
-'"\n                            class="social-share share-twitter"\n                            data-itemid="'+
+' @zeega"\n                            class="social-share share-twitter"\n                            data-itemid="'+
 ( item_id )+
 '"\n                            target="blank">\n                        <i class="zitem-twitter zitem-30 color"></i>\n                    </a>\n                    <a href="http://www.facebook.com/sharer.php?u='+
 ( webRoot )+
