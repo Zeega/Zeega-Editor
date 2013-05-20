6c6462b3c1cc316b664421fb1c1abcafd5faa1c9
jbergen <jbergen@gmail.com>
2013-05-20 12:33:18 -0700

    updated intro modal with content

--- app/modules/intro-modal/intro-modal.view.js
+++ app/modules/intro-modal/intro-modal.view.js
@@ -16,18 +16,27 @@ function( app ) {
             $("body").append( this.el );
             $("#main").addClass("modal");
             this.render();
+            this.$el.fadeIn("fast");
         },
 
         events: {
-            "click .next": "next"
+            "click .next": "next",
+            "click .finish": "finish"
         },
 
         next: function() {
-            
+            this.$(".step-1").fadeOut(function() {
+                this.$(".step-2").fadeIn();
+            }.bind( this ));
+        },
+
+        finish: function() {
+            this.hide();
         },
 
         hide: function() {
-            this.fadeOut(function() {
+            $("#main").removeClass("modal");
+            this.$el.fadeOut(function() {
                 this.remove();
             }.bind( this ));
         }
