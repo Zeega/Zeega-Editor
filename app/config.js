ec7d03cedb71d738a9de93ba8a14addb423df902
jbergen <jbergen@gmail.com>
2013-06-04 23:09:51 -0700

    config clean

--- app/config.js
+++ app/config.js
@@ -1,14 +1,8 @@
-// Set the require.js configuration for your application.
 require.config({
 
-  // Initialize the application with the main application file and the JamJS
-  // generated configuration file.
-
-  // Release
   deps: ["../vendor/jam/require.config", "main"],
 
   paths: {
-
     jqueryUI: "../assets/js/plugins/jquery-ui/js/jquery-ui-1.10.1.custom",
 
     zeega_parser: "zeega-parser",
