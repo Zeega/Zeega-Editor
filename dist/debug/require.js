cb717efca4934980cca80bf7857de799ba9801a0
James Burns <jameburn@gmail.com>
2013-06-12 11:49:11 -0700

    release

--- dist/debug/require.js
+++ dist/debug/require.js
@@ -45126,7 +45126,7 @@ function( app, ItemModel, CollectionView, Collection, ItemCollectionViewer ) {
             var args = this.get("urlArguments");
 
             args.before = new Date().getTime();
-            args.tag = query.replace( " ", "-" );
+            args.tag = query.replace( " ", "-" ) + "-gif";
             args.url = "http://www.tumblr.com/tagged/" + args.tag + "/before/" + args.before;
 
             this.set("urlArguments", args );
