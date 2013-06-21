7d78d8cb6dcc8ca5dd25654c21ab49667423de80
jbergen <jbergen@gmail.com>
2013-06-21 13:16:38 -0700

    added spinner to fix broken preview

--- app/app.js
+++ app/app.js
@@ -1,6 +1,8 @@
 define([
+    "engineVendor/spin",
     "backbone.layoutmanager"
-], function() {
+
+], function( Spinner ) {
 
     var meta = $("meta[name=zeega]");
 
@@ -34,8 +36,27 @@ define([
             // other things can be done here as well
             this.trigger( event, args );
         }
-    
     };
+
+    var opts = {
+        lines: 13, // The number of lines to draw
+        length: 10, // The length of each line
+        width: 4, // The line thickness
+        radius: 30, // The radius of the inner circle
+        corners: 1, // Corner roundness (0..1)
+        rotate: 0, // The rotation offset
+        direction: 1, // 1: clockwise, -1: counterclockwise
+        color: '#FFF', // #rgb or #rrggbb
+        speed: 1, // Rounds per second
+        trail: 60, // Afterglow percentage
+        shadow: false, // Whether to render a shadow
+        hwaccel: false, // Whether to use hardware acceleration
+        className: 'spinner', // The CSS class to assign to the spinner
+        zIndex: 2e9, // The z-index (defaults to 2000000000)
+        top: 'auto', // Top position relative to parent in px
+        left: 'auto' // Left position relative to parent in px
+    };
+    app.spinner = new Spinner(opts);
 
     // Localize or create a new JavaScript Template object.
     var JST = window.JST = window.JST || {};
