8bc6cad95a30ba84b8637bb04acdfbc881f9b6dc
James Burns <jameburn@gmail.com>
2013-06-21 13:42:56 -0700

    add spinner to editor app

--- app/app.js
+++ app/app.js
@@ -1,6 +1,7 @@
 define([
+    "engineVendor/spin",
     "backbone.layoutmanager"
-], function() {
+], function( Spinner ) {
 
     var meta = $("meta[name=zeega]");
 
@@ -33,7 +34,25 @@ define([
         emit: function( event, args ) {
             // other things can be done here as well
             this.trigger( event, args );
-        }
+        },
+
+        spinner: new Spinner({
+            lines: 13, // The number of lines to draw
+            length: 7, // The length of each line
+            width: 4, // The line thickness
+            radius: 20, // The radius of the inner circle
+            corners: 1, // Corner roundness (0..1)
+            rotate: 0, // The rotation offset
+            color: '#fff', // #rgb or #rrggbb
+            speed: 1, // Rounds per second
+            trail: 60, // Afterglow percentage
+            shadow: false, // Whether to render a shadow
+            hwaccel: false, // Whether to use hardware acceleration
+            className: 'spinner', // The CSS class to assign to the spinner
+            zIndex: 100, // The z-index (defaults to 2000000000)
+            top: 'auto', // Top position relative to parent in px
+            left: 'auto' // Left position relative to parent in px
+        })
     
     };
 
