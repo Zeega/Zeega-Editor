eef9bd65bec4492fdf6799b2baaebd635c483fff
jbergen <jbergen@gmail.com>
2013-07-08 13:43:27 -0700

    save indicator saved

--- app/app.js
+++ app/app.js
@@ -34,10 +34,39 @@ define([
         emit: function( event, args ) {
             // other things can be done here as well
             this.trigger( event, args );
+        },
+
+        _saving: 0,
+        _saveIndicatorTimeout: null,
+
+        onSaveStart: function( target ) {
+
+            if ( this._saveIndicatorTimeout ) {
+                clearTimeout( this._saveIndicatorTimeout );
+            }
+
+            $(".saving-indicator").text("saving");
+            this._saving++;
+        },
+
+        onSaveSuccess: function( target, opts ) {
+            this._saving--;
+
+            if( this._saving < 1 ) {
+                $(".saving-indicator").text("all changes saved");
+                this._saveIndicatorTimeout = setTimeout( function() {
+                    $(".saving-indicator").empty();
+                }, 2000 );
+            }
+        },
+        
+        onSaveError: function() {
+
         }
+
     };
 
-    var opts = {
+    var spinnerOptions = {
         lines: 13, // The number of lines to draw
         length: 10, // The length of each line
         width: 4, // The line thickness
@@ -55,7 +84,7 @@ define([
         top: 'auto', // Top position relative to parent in px
         left: 'auto' // Left position relative to parent in px
     };
-    app.spinner = new Spinner(opts);
+    app.spinner = new Spinner( spinnerOptions );
 
     // Localize or create a new JavaScript Template object.
     var JST = window.JST = window.JST || {};
@@ -66,6 +95,12 @@ define([
     Backbone.Model.prototype.put = function() {
         var args = [].slice.call( arguments ).concat([ { silent: true } ]);
         return this.set.apply( this, args );
+    };
+
+    // events that trigger the save indicator on the editor interface
+    Backbone.Model.prototype.initSaveEvents = function() {
+        this.on("request", app.onSaveStart, app );
+        this.on("sync", app.onSaveSuccess, app );
     };
 
     $ = jQuery;
