4a3f8c32d76714a13cdb2bd3c49d0e44047462fc
James Burns <jameburn@gmail.com>
2013-07-24 11:25:09 -0700

    release

--- dist/debug/require.js
+++ dist/debug/require.js
@@ -44765,8 +44765,8 @@ function( app, Status, Layout, ZeegaParser, Analytics ) {
 
         insertLayout: function() {
 
-            // var location = app.metadata.root == "/" ? app.metadata.root + "editor/" + app.project.id : "/" + app.metadata.root + "editor/" + app.project.id;
-            // window.history.pushState("", "", location );
+            var location = app.metadata.root == "/" ? app.metadata.root + "editor/" + app.project.id : "/" + app.metadata.root + "editor/" + app.project.id;
+            window.history.pushState("", "", location );
 
             app.layout = new Layout();
             app.layout.render();
