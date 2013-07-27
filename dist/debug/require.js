b2656c7abdba7b1419962bb0b19f8cdfc86ead27
James Burns <jameburn@gmail.com>
2013-07-26 18:06:35 -0700

    release

--- dist/debug/require.js
+++ dist/debug/require.js
@@ -34935,7 +34935,7 @@ function( app, Layer, Visual ){
 
         isAnimated: function(){
 
-            if(!_.isNull(this.model.getAttr("zga_uri")) && !_.isNull(this.model.getAttr("zga_uri"))){
+            if(!_.isNull(this.model.getAttr("zga_uri")) && !_.isUndefined(this.model.getAttr("zga_uri"))){
                 return true;
             } else {
                 return false;
