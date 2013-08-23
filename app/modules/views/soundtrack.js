bd01cb786cedc31193b323f4b8c87424dcdc85be
jbergen <jbergen@gmail.com>
2013-08-23 12:11:05 -0700

    fixed broken preview after adding audio

--- app/modules/views/soundtrack.js
+++ app/modules/views/soundtrack.js
@@ -154,7 +154,7 @@ function( app, Viewer ) {
         },
 
         pause: function() {
-            if ( this.canPlayMpeg() && this.model ) {
+            if ( this.canPlayMpeg() && this.model && this.model.visual.audio ) {
                 this.model.visual.audio.pause();
             }
         },
