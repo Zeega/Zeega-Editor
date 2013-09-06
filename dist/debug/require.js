8a434d96370861d8023b89ebc361f4fc69cbcc79
James Burns <jameburn@gmail.com>
2013-09-05 17:03:06 -0700

    release

--- dist/debug/require.js
+++ dist/debug/require.js
@@ -42849,17 +42849,16 @@ function( app, ItemView, Asker ) {
             imageData.append( "file", fileInput.files[0] );
 
             var updateProgress = function( e ){
-                var w = e.loaded * 121 / e.total;
+                var w = e.loaded * 141 / e.total;
 
-                if( e.loaded * 2 > e.total){
+                if( e.loaded * 1.5 > e.total){
                     $(".upload-instructions").html("optimizing...");
                 }
 
                 _this.$('.upload-progress').clearQueue().animate ({ "width": w + "px"}, 1000);
                 if(  w == 141 ) {
-                    _this.$('.upload-progress').clearQueue().animate ({ "width": "283px"}, 10000);
+                    _this.$('.upload-progress').clearQueue().animate ({ "width": "270px"}, 10000);
                 }
-
             };
 
             $.ajax({
